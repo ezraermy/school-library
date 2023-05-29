90a5e7a8f9b757ccebc4ef710191f3f8bb8da36b
getssh <gtamrat33@gmail.com>
2023-05-29 23:28:16 +0300

    Add main method as an entry point

--- main.rb
+++ main.rb
@@ -1,1 +1,18 @@
+require_relative 'app'
+require_relative 'menu'
 
+def main
+  app = App.new
+  menu = Menu.new(app)
+
+  loop do
+    menu.display_menu_options
+    choice = gets.chomp.to_i
+
+    menu.handle_choice(choice)
+
+    break if choice == 7
+  end
+end
+
+main
