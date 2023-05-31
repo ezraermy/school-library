86e8dbb0cfb1e3b2ed8048de7f7af183517e5bcf
getssh <gtamrat33@gmail.com>
2023-06-01 02:09:30 +0300

    Add book test for author

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,8 +1,14 @@
+require_relative '../book'
+
 describe Book do
   context 'when initialized' do
     it 'has a title' do
       book = Book.new('title', 'author')
       expect(book.title).to eq('title')
+    end
+    it 'has an author' do
+      book = Book.new('title', 'author')
+      expect(book.author).to eq('author')
     end
   end
 end
